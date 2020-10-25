b172e280d6a72e6414addc930acc94f604596b88
ananya-R <royananya71@gmail.com>
2020-10-25 22:01:15 +0530

    updated

---
+++ build/precache-manifest.1198c691d88d29ec53183e31b16644e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f32527239acc4ebc13cbdae7434d0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3a8a46811b5f19b9d70",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "b37618341a9f91ec0d3b",
+    "url": "/static/css/main.819791d0.chunk.css"
+  },
+  {
+    "revision": "e3a8a46811b5f19b9d70",
+    "url": "/static/js/2.12acc934.chunk.js"
+  },
+  {
+    "revision": "c69fcf8670b313a5daf5b087e062c182",
+    "url": "/static/js/2.12acc934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b37618341a9f91ec0d3b",
+    "url": "/static/js/main.c0634bb0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
